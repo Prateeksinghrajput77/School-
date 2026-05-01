d2ab98d1d9639f8c9cc5ae669772deabfc0c063f
Prateeksinghrajput77 <prateeksinghrajput250@gmail.com>
2026-05-01 14:40:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Just for small dynamic effect
+console.log("School website loaded successfully");
+
+// Optional: change topper score dynamically
+document.getElementById("score").style.color = "green";
